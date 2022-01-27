68d4bd85b5a2eb5bdadbf7d7de43797e7d682955
superkrzysio <superkrzysio@protonmail.com>
2022-01-27 21:37:19 +0100

    Remove stub libs

--- RPi/__init__.py
+++
@@ -1,1 +0,0 @@
-print("Warning! Using stub version of RPi.GPIO for development. Remove this module on RPi")
--- picamera/__init__.py
+++
@@ -1,3 +0,0 @@
-from picamera.camera import PiCamera
-
-print("Warning! Using stub version of picamera for development. Remove this module on RPi")
